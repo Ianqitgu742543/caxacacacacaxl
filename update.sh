f9ae83587a3dc3ad620d0fe55f2dc7034ad99cd8
Ianqitgu742543 <87284724+Ianqitgu742543@users.noreply.github.com>
2021-08-12 23:48:26 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -10,7 +10,7 @@ fi
 echo "Start Update"
 #cd /usr/bin
 rm /usr/bin/menu
-wget -q https://raw.githubusercontent.com/ZzZzZzZXCoo/error/main/install.sh && chmod +x install.sh && sed -i -e 's/\r$//' install.sh && ./install.sh
+wget -q https://raw.githubusercontent.com/Ianqitgu742543/bangsat/main/install.sh && chmod +x install.sh && sed -i -e 's/\r$//' install.sh && ./install.sh
 rm install.sh
 clear
 echo " Update Successfull"
